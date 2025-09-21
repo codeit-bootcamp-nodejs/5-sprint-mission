05a75f33145f19bd2aacff7b5526bef42f549b7d
juho-creator <juhokim2018@gmail.com>
2025-09-21 19:20:16 +0900

    [Feat] 응답 dto 생성함

---
+++ 02-controller/res-dto/comment.res.dto.js
@@ -0,0 +1,19 @@
+export class CommentResDto {
+
+    id
+    content
+    createdAt
+    updatedAt
+    articleId
+    productId
+
+
+    constructor({ id, content, createdAt, updatedAt, articeId = "", productId = "" }) {
+        this.id = id;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.articleId = articeId;
+        this.productId = productId;
+    }
+}
