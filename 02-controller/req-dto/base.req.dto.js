2ed92143538f3763beef5fc26882e949b7f24299
juho-creator <juhokim2018@gmail.com>
2025-09-21 19:19:42 +0900

    [Feat] 요청 dto 생성함

---
+++ 02-controller/req-dto/base.req.dto.js
@@ -0,0 +1,36 @@
+export class BaseReqDto {
+    #headers
+    #params
+    #query
+    #body
+
+    constructor({
+        headers = {},
+        params = {},
+        query = {},
+        body = {}
+    }) {
+        this.#headers = headers;
+        this.#query = query;
+        this.#params = params;
+        this.#body = body;
+    }
+
+    get headers() {
+        return this.#headers;
+    }
+
+
+    get params() {
+        return this.#params;
+    }
+
+
+    get body() {
+        return this.#body;
+    }
+
+    get query() {
+        return this.#query;
+    }
+}
