cb6f2302da0857aab7783ca566049c82583677ff
918-jihye <wlgp181004@gmail.com>
2025-10-26 19:12:41 +0900

    [Feat] 파일 lib/token & upload 구현

---
+++ src/lib/token.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+
+export const signAccess = (userId) =>
+  jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN || "15m" });
+
+export const signRefresh = (userId) =>
+  jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || "7d" });
+
+export const verifyAccess = (token) => jwt.verify(token, process.env.JWT_SECRET);
+export const verifyRefresh = (token) => jwt.verify(token, process.env.JWT_REFRESH_SECRET);
