6b6361651414b52a9088c7bd1302397f5f580e7c
GhostGN95 <wcxie1234@gmail.com>
2025-12-08 22:45:25 +0900

    스프린트 미션8 초기 설정 완료

---
+++ sprint-mission-8/prisma/seed.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const hashedPassword = await bcrypt.hash("1234", 10);
+  const user = await prisma.user.create({
+    data: {
+      email: "asd@asd.com",
+      nickname: "asd",
+      password: hashedPassword,
+    },
+  });
+
+  await prisma.product.createMany({
+    data: [
+      {
+        name: "노트북",
+        description: "게이밍 노트북",
+        price: 3000000,
+        tags: ["전자제품", "게임"],
+        authorId: user.id,
+      },
+      {
+        name: "축구공",
+        description: "공식 경기용 축구공",
+        price: 50000,
+        tags: ["스포츠", "축구"],
+        authorId: user.id,
+      },
+    ],
+  });
+
+  await prisma.article.createMany({
+    data: [
+      {
+        title: "첫 번째 글",
+        content: "자유게시판에 오신 것을 환영합니다!",
+        authorId: user.id,
+      },
+      {
+        title: "두 번째 글",
+        content: "오늘은 날씨가 많이 서늘해졌어요.",
+        authorId: user.id,
+      },
+    ],
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ sprint-mission-8/src/routes/comments.ts
@@ -0,0 +1,62 @@
+import { Router } from "express";
+import authMiddleware from "../middlewares/auth.middleware";
+import { validateComment } from "../middlewares/validator/validate.comment";
+import { ProductRepository } from "../repo/product.repository";
+import { ArticleRepository } from "../repo/article.repository";
+import { CommentRepository } from "../repo/comment.repository";
+import { CommentService } from "../service/comment.service";
+import { CommentController } from "../controller/comment.controller";
+
+const productRepository = new ProductRepository();
+const articleRepository = new ArticleRepository();
+const commentRepository = new CommentRepository();
+
+const commentService = new CommentService(
+  commentRepository,
+  productRepository,
+  articleRepository
+);
+
+const commentController = new CommentController(commentService);
+
+const router = Router();
+
+router.post(
+  "/product/:productId",
+  authMiddleware,
+  validateComment,
+  commentController.createProductComment,
+);
+router.get("/product/:productId", commentController.getProductComments);
+router.patch(
+  "/product/:commentId",
+  authMiddleware,
+  validateComment,
+  commentController.updateProductComment,
+);
+router.delete(
+  "/product/:commentId",
+  authMiddleware,
+  commentController.deleteProductComment,
+);
+
+router.post(
+  "/article/:articleId",
+  authMiddleware,
+  validateComment,
+  commentController.createArticleComment,
+);
+router.get("/article/:articleId", commentController.getArticleComments);
+router.patch(
+  "/article/:commentId",
+  authMiddleware,
+  validateComment,
+  commentController.updateArticleComment,
+);
+router.delete(
+  "/article/:commentId",
+  authMiddleware,
+  commentController.deleteArticleComment,
+);
+
+export default router;
