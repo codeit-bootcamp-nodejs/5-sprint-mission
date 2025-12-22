5f26d0320d541239855dc4408d3d8635cf8b6b4b
juho-creator <juhokim2018@gmail.com>
2025-12-22 21:05:12 +0900

    refactor: 불필요한 파일 제거

--- src/02-domain/entity/product.like.ts
+++
@@ -1,8 +0,0 @@
-export const ProductLike = (params: { userId: string; productId: string }) => {
-  return {
-    userId: params.userId,
-    productId: params.productId,
-  };
-};
-
-export type PersistedProductLike = ReturnType<typeof ProductLike>;
