d50536423d80181501b286e18f2739b9f502399d
juho-creator <juhokim2018@gmail.com>
2025-12-30 14:33:22 +0900

    fix: 서비스 유닛 테스트시 db 접근 x

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,17 @@
 export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/*.test.ts"],
-  setupFilesAfterEnv: ["<rootDir>/src/test/jest/setup.ts"],
-  verbose: true,
+  projects: [
+    {
+      displayName: "api",
+      preset: "ts-jest",
+      testEnvironment: "node",
+      testMatch: ["<rootDir>/src/test/jest/api/*.test.ts"],
+      setupFilesAfterEnv: ["<rootDir>/src/test/jest/setup.ts"],
+    },
+    {
+      displayName: "service",
+      preset: "ts-jest",
+      testEnvironment: "node",
+      testMatch: ["<rootDir>/src/test/jest/service/*.test.ts"],
+    },
+  ],
 };
